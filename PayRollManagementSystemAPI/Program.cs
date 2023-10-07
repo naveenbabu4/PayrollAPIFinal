3b9ec7da3065b6b4ee99c23e289620ec5e433723
naveen <bsa.naveenbabu@gmail.com>
2023-10-07 12:56:38 +0530

    * cors

--- PayRollManagementSystemAPI/Program.cs
+++ PayRollManagementSystemAPI/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
         builder => builder
-            .AllowAnyOrigin()
+            .WithOrigins("*")
             .AllowAnyMethod()
             .AllowAnyHeader()
             );
